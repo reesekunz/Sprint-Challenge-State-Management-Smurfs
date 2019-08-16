9f00b81bf5a55439c13e2be4cd25ece253610edb
Reese Kunz <reesekunz21@gmail.com>
2019-08-16 09:05:19 -0700

    set up store and provider

---
+++ smurfs/src/components/reducers/index.js
@@ -0,0 +1,10 @@
+const initialState = {
+  arrayName: []
+};
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,4 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { createStore } from “redux”;
+import { reducer } from “./reducers”;
+import { Provider } from “react-redux”;
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>, document.getElementById("root"));
